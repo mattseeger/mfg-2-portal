57b71d97e7922d56b8b6c7866b8040e6f21688f6
mseeger <mseeger@mseeger-ltm.internal.salesforce.com>
2017-09-21 08:31:07 -0500

    Added Method to Use LoadConfiguration. Added Method to create lead and link quote

--- lib/SFDCConnector.php
+++ lib/SFDCConnector.php
@@ -81,6 +81,79 @@ class SFDCConnector {
         return $query_response;
     }
     
+    public function createLead($first, $last, $email, $phone, $company, $city, $state, $description='Web Lead', $quoteId = NULL){
+        $reqXML = new DOMDocument();
+	$s_Envelope = $reqXML->createElementNS ("http://schemas.xmlsoap.org/soap/envelope/" , "s:Envelope");
+	$reqXML->appendChild($s_Envelope);
+
+	$s_Header = $reqXML->createElementNS ("http://schemas.xmlsoap.org/soap/envelope/" , "s:Header");
+	$s_Envelope->appendChild($s_Header);
+
+	$s_Body = $reqXML->createElementNS ("http://schemas.xmlsoap.org/soap/envelope/" , "s:Body");
+	$s_Envelope->appendChild($s_Body);
+
+	$sf_SeassionHeader = $reqXML->createElementNS ("urn:enterprise.soap.sforce.com" , "sf:SessionHeader");
+	$s_Header->appendChild($sf_SeassionHeader);
+
+	$sf_sessionId = $reqXML->createElementNS ("urn:enterprise.soap.sforce.com" , "sf:sessionId", $this->sessionId);
+	$sf_SeassionHeader->appendChild($sf_sessionId);
+
+	$sf_create = $reqXML->createElementNS ("urn:enterprise.soap.sforce.com" , "sf:create");
+	$s_Body->appendChild($sf_create);
+
+	$sf_sObjects = $reqXML->createElementNS ("urn:enterprise.soap.sforce.com" , "sf:sObjects");
+	$sf_sObjects->setAttributeNS ("http://www.w3.org/2001/XMLSchema-instance" , "xsi:type" , "Lead");
+	$sf_create->appendChild($sf_sObjects);
+        
+        $leadFirst = $reqXML->createElement('FirstName', $first);
+        $sf_sObjects->appendChild($leadFirst);
+        
+        $leadLast = $reqXML->createElement('LastName', $last);
+        $sf_sObjects->appendChild($leadLast);
+        
+        $leadEmail = $reqXML->createElement('Email', $email);
+        $sf_sObjects->appendChild($leadEmail);
+        
+        $leadPhone = $reqXML->createElement('Phone', $phone);
+        $sf_sObjects->appendChild($leadPhone);
+        
+        $leadCompany = $reqXML->createElement('Company', $company);
+        $sf_sObjects->appendChild($leadCompany);
+        
+        $leadCity = $reqXML->createElement('City', $city);
+        $sf_sObjects->appendChild($leadCity);
+        
+        $leadState = $reqXML->createElement('State', $state);
+        $sf_sObjects->appendChild($leadState);
+        
+        $leadSource = $reqXML->createElement('LeadSource', 'Web');
+        $sf_sObjects->appendChild($leadSource);
+        
+        $leadDescription = $reqXML->createElement('Description', $description);
+        $sf_sObjects->appendChild($leadDescription);
+        
+        $leadWebLead = $reqXML->createElement('Web_Lead__c', true);
+        $sf_sObjects->appendChild($leadWebLead);
+        
+        if($quoteId !== NULL){
+            $leadQuote = $reqXML->createElement('Web_Quote__c', $quoteId);
+            $sf_sObjects->appendChild($leadQuote);
+        }
+
+        $ch = curl_init($this->serverURL);
+	curl_setopt( $ch, CURLOPT_POST, true );
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml', 'SOAPAction: ""'));
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, $reqXML->saveXML());
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	$resXML = new DOMDocument();
+	$resXML->loadXML($result);
+        $leadId = $resXML->getElementsByTagName("id")->item(0)->textContent;
+        return $leadId;       
+    }
+    
     /**
      * 
      * @param String $productID
@@ -100,5 +173,18 @@ class SFDCConnector {
         }
         
         return $LoadProductByIDResponse->result;
+    }
+    
+    public function LoadConfigurationByID($productID){
+        $LoadConfigurationByIDResponse = NULL;
+        $soap_data = new stdClass();
+        $soap_data->productId = $productID;
+        
+        try{
+            $LoadConfigurationByIDResponse = $this->cpqSoap->ConfigLoader($soap_data);
+        } catch (Exception $ex) {
+
+        }
+        return $LoadConfigurationByIDResponse->result;
     }
 }
