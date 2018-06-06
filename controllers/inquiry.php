c4542d94a9c35a2599d0a4ad6421df050f1e4a41
Matthew Seeger <mseeger@mseeger-ltm.internal.salesforce.com>
2018-06-06 09:41:45 -0500

    Added a hard coded Quote ID to do a lead gen story

--- controllers/inquiry.php
+++ controllers/inquiry.php
@@ -14,7 +14,7 @@ class inquiry extends Controller{
         $sfdc = new SFDCConnector();
         $configJSON = $_POST['configurationJSON'];
         //$quoteId = $sfdc->CreateOrphanQuote();
-        $leadId = $sfdc->createLead($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['phone'], $_POST['company'], $_POST['city'], $_POST['state'], $_POST['description'], 'a0l46000001JIReAAO');
+        $leadId = $sfdc->createLead($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['phone'], $_POST['company'], $_POST['city'], $_POST['state'], $_POST['description'], 'a0l46000002BMaQAAW');
         $this->view->renderHeader();
         $this->view->render("thanks", $_POST['first_name']);
         $this->view->renderFooter();
